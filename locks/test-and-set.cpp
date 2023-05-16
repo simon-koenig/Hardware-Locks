85d9053b3ff53a2906dc6a735c7b67dd16fb532a
Viktor Beck <e11713110@student.tuwien.ac.at>
2023-05-16 15:57:28 +0200

    implemented a simple test to check for mutual exclusion

--- locks/test-and-set.cpp
+++ locks/test-and-set.cpp
@@ -5,28 +5,12 @@
 
 std::atomic_flag locked = ATOMIC_FLAG_INIT; // atomic register
 
-void lock() {
+void tas_lock() {
     while (locked.test_and_set()){}
     return;
 }
-void unlock () {
+void tas_unlock () {
     locked.clear();
-    std::cout << "unlocked: " << omp_get_thread_num() << std::endl;
 }
 
-int main ()
-{
-    std::cout << "pre lock: " << std::endl;
-    unsigned int microsecond = 1000000;
-    #pragma omp parallel
-    {
-        std::cout << omp_get_thread_num() << std::endl;
-        usleep(1 * microsecond);//sleeps for 1 second
-        lock();
-        usleep(0.5 * microsecond);//sleeps for 0.5 second
-        std::cout << "in lock: " << omp_get_thread_num() << std::endl;
-        usleep(0.5 * microsecond);//sleeps for 0.5 second
-        unlock();
-    }
-    return 0;
-}
+
---
+++ tests/simple_mutex_test.cpp
@@ -0,0 +1,21 @@
+#include "../locks/test-and-set.cpp"
+
+int main ()
+{
+    unsigned int microsecond = 1000000;
+
+    std::cout << "pre lock: " << std::endl;
+
+    #pragma omp parallel
+    {
+        std::cout << omp_get_thread_num() << std::endl;
+        usleep(1 * microsecond);//sleeps for 1 second
+        tas_lock();
+        usleep(0.5 * microsecond);//sleeps for 0.5 second
+        std::cout << "in lock: " << omp_get_thread_num() << std::endl;
+        usleep(0.5 * microsecond);//sleeps for 0.5 second
+        tas_unlock();
+        std::cout << "unlocked: " << omp_get_thread_num() << std::endl;
+    }
+    return 0;
+}
