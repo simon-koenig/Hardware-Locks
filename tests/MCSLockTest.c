5d15471a7b78f22ecefdcc0b4f6df6a506bfe189
MichaelKetter <101721998+MichaelKetter@users.noreply.github.com>
2023-06-07 11:06:19 +0200

    Rename MCS-lock_test.c to MCSLockTest.c and change to standardized lock-handling structure

--- tests/MCSLockTest.c
+++ tests/MCSLockTest.c
@@ -1,34 +1,31 @@
-#include "../locks/MCS.c"
+#include "../locks/MCSLock.c"
 
 int main ()
 {
     unsigned int microsecond = 1000000;
-    omp_set_num_threads(8);
+    omp_set_num_threads(8); // Has to be <= than the number of flags defined in the array
+    Lock LOCK;
+    init(&LOCK);
 
-    // Declare Mutex
-    struct MCS_Lock MCS_LOCK;
-    // Initialize Mutex
-    MCS_init(&MCS_LOCK);
-
-    printf("Before Lock\n");
-    #pragma omp parallel shared(MCS_LOCK)
+    #pragma omp parallel shared(LOCK)
     {   /* Only master thread does this */
         if (omp_get_thread_num()==0) {
             printf("Number of threads = %d\n", omp_get_num_threads());
             }
+       
         usleep(1 * microsecond); //sleeps for 1 second
         // Acquire the lock
-        MCS_lock(&MCS_LOCK);
+        lock(&LOCK);
         usleep(0.5 * microsecond);//sleeps for 0.5 second
         printf("Hej thread = %d aquired the lock \n ",omp_get_thread_num());
         usleep(0.5 * microsecond);//sleeps for 0.5 second
         // Release the lock 
-        MCS_unlock(&MCS_LOCK);
+        unlock(&LOCK);
         printf("Hej thread = %d has unlocked \n ",omp_get_thread_num());
-   
-    }
-    MCS_destroy_lock(&MCS_LOCK);
-    printf("After Lock\n");
-    return 0;
 
+    }
+    
+    // Free the lock
+    destroy(&LOCK);
+    return 0;
 }
