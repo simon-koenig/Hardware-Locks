79ec33b5509e70e16391cd92ce7bf48a49bd4593
MichaelKetter <101721998+MichaelKetter@users.noreply.github.com>
2023-06-07 11:04:27 +0200

    Rename Helmlock_test.c to HelmLockTest.c and change to standardized lock-handling structure

--- tests/HelmLockTest.c
+++ tests/HelmLockTest.c
@@ -1,33 +1,27 @@
-#include "../locks/Helmlock.c"
+#include "../locks/HelmLock.c"
 
 int main ()
 {
     unsigned int microsecond = 1000000;
     omp_set_num_threads(8);
 
-    // Declare Mutex
-    struct Helmlock_Lock HELMLOCK;
-    // Initialize Mutex
-    Helmlock_init(&HELMLOCK);
 
-    printf("Before Lock\n");
-    #pragma omp parallel shared(HELMLOCK)
+    printf("Before Lock");
+    #pragma omp parallel
     {   /* Only master thread does this */
         if (omp_get_thread_num()==0) {
             printf("Number of threads = %d\n", omp_get_num_threads());
             }
-        usleep(1 * microsecond); //sleeps for 1 second
-        // Acquire the lock
-        Helmlock_lock(&HELMLOCK);
+        
+        // printf("Hej from thread = %d\n",omp_get_thread_num());
+        usleep(1 * microsecond);//sleeps for 1 second
+        lock();
         usleep(0.5 * microsecond);//sleeps for 0.5 second
         printf("Hej thread = %d aquired the lock \n ",omp_get_thread_num());
         usleep(0.5 * microsecond);//sleeps for 0.5 second
-        // Release the lock 
-        Helmlock_unlock(&HELMLOCK);
+        unlock();
         printf("Hej thread = %d has unlocked \n ",omp_get_thread_num());
-   
     }
     printf("After Lock\n");
     return 0;
-
 }
