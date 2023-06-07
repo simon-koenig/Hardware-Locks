0b4670b556a97705adbde777ece96c13491ca58c
MichaelKetter <101721998+MichaelKetter@users.noreply.github.com>
2023-06-07 14:56:53 +0200

    Rename array-lock-test.c to ArrayLockTest.c and modify structure for standardized lock-handling

--- tests/ArrayLockTest.c
+++ tests/ArrayLockTest.c
@@ -1,27 +1,33 @@
-#include "../locks/array-lock.c"
+#include "../locks/ArrayLock.c"
 
 int main ()
 {
     unsigned int microsecond = 1000000;
     omp_set_num_threads(8); // Has to be <= than the number of flags defined in the array
+    // Declare Mutex
+    Lock LOCK;
+    // Initialize Mutex
+    init(&LOCK);
 
-
-    printf("Before Lock");
-    #pragma omp parallel
+    #pragma omp parallel shared(LOCK)
     {   /* Only master thread does this */
         if (omp_get_thread_num()==0) {
             printf("Number of threads = %d\n", omp_get_num_threads());
             }
-        
-        // printf("Hej from thread = %d\n",omp_get_thread_num());
-        usleep(1 * microsecond);//sleeps for 1 second
-        lock();
+       
+        usleep(1 * microsecond); //sleeps for 1 second
+        // Acquire the lock
+        lock(&LOCK);
         usleep(0.5 * microsecond);//sleeps for 0.5 second
         printf("Hej thread = %d aquired the lock \n ",omp_get_thread_num());
         usleep(0.5 * microsecond);//sleeps for 0.5 second
-        unlock();
+        // Release the lock 
+        unlock(&LOCK);
         printf("Hej thread = %d has unlocked \n ",omp_get_thread_num());
+
     }
-    printf("After Lock\n");
+    
+    // Free the lock
+    destroy(&LOCK);
     return 0;
 }
