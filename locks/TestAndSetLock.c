1a1f5ed62a3368c641236e1b48fe464c3adee123
MichaelKetter <101721998+MichaelKetter@users.noreply.github.com>
2023-06-07 11:01:19 +0200

    Rrename test-and-set.c to TestAndSetLock.c and adapt Lock structure to be coherent with the other Locks

---
+++ locks/TestAndSetLock.c
@@ -0,0 +1,25 @@
+#include <stdatomic.h>
+#include <unistd.h>
+#include <omp.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct Lock {
+atomic_bool lock_flag;
+} Lock;
+
+void init(Lock* self){
+    atomic_store_explicit(&self->lock_flag, 0, memory_order_release);
+}
+
+void lock(Lock* self) {
+    while (atomic_exchange_explicit(&self->lock_flag, 1, memory_order_acquire)){};
+}
+
+void unlock(Lock* self) {
+    atomic_store_explicit(&self->lock_flag, 0, memory_order_release);
+}
+
+void destroy(Lock* self){
+    atomic_store_explicit(&self->lock_flag, 0, memory_order_release);
+}
