d150e9885228142a739c2611f6d0cf0f8cf1c7f6
MichaelKetter <101721998+MichaelKetter@users.noreply.github.com>
2023-06-07 14:53:09 +0200

    Rename array-lock.c to ArrayLock.c and change structure so its compatible with standardized calling method

---
+++ lock/ArrayLock.c
@@ -0,0 +1,41 @@
+#include <stdatomic.h>
+#include <unistd.h>
+#include <omp.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+typedef struct Lock {
+  bool* flags;
+  // Making an atomic counter
+  _Atomic int tail;
+} Lock;
+
+// Thread local variable 
+__thread int mySlot ;
+
+void init(Lock* self){
+    self->flags = malloc(sizeof(bool)*omp_get_max_threads());
+    printf("number of threads = %i \n", omp_get_max_threads());
+    for (int i=0; i<omp_get_max_threads(); i++) { self->flags[i] = false; }
+    self->flags[0] = true;
+    atomic_store(&self->tail, 0);   
+    for (int i=0; i<omp_get_max_threads(); i++) { printf("thread %i has flag %i \n", i, self->flags[i]); }
+}
+
+void lock(Lock* self) {
+    mySlot = __sync_fetch_and_add(&self->tail, 1) % omp_get_num_threads();
+    
+    while (!self->flags[mySlot]) {
+        // Wait until the flag at mySlot is set to true
+    }
+}
+void unlock(Lock* self) {
+    self->flags[mySlot] = false;
+    self->flags[(mySlot + 1) % omp_get_num_threads()] = true;
+}
+
+void destroy(Lock* self){
+    free(self->flags);
+}
+
