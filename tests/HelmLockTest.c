4834d7fc99e4cd173867875dc74e755045efb035
MichaelKetter <101721998+MichaelKetter@users.noreply.github.com>
2023-06-07 11:07:24 +0200

    Update HelmLockTest.c - repair mistake in last commit

--- tests/HelmLockTest.c
+++ tests/HelmLockTest.c
@@ -1,27 +1,30 @@
 #include "../locks/HelmLock.c"
-
 int main ()
 {
     unsigned int microsecond = 1000000;
-    omp_set_num_threads(8);
+    omp_set_num_threads(8); // Has to be <= than the number of flags defined in the array
+    Lock LOCK;
+    init(&LOCK);
 
-
-    printf("Before Lock");
-    #pragma omp parallel
+    #pragma omp parallel shared(LOCK)
     {   /* Only master thread does this */
         if (omp_get_thread_num()==0) {
             printf("Number of threads = %d\n", omp_get_num_threads());
             }
-        
-        // printf("Hej from thread = %d\n",omp_get_thread_num());
-        usleep(1 * microsecond);//sleeps for 1 second
-        lock();
+       
+        usleep(1 * microsecond); //sleeps for 1 second
+        // Acquire the lock
+        lock(&LOCK);
         usleep(0.5 * microsecond);//sleeps for 0.5 second
         printf("Hej thread = %d aquired the lock \n ",omp_get_thread_num());
         usleep(0.5 * microsecond);//sleeps for 0.5 second
-        unlock();
+        // Release the lock 
+        unlock(&LOCK);
         printf("Hej thread = %d has unlocked \n ",omp_get_thread_num());
+
     }
-    printf("After Lock\n");
+    
+    // Free the lock
+    destroy(&LOCK);
     return 0;
 }
