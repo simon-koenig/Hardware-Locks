fc56468b01979d32d4e2a2ea106986c8958d92f8
Viktor Beck <e11713110@student.tuwien.ac.at>
2023-06-13 15:56:47 +0200

    restructured project a little bit

---
+++ Makefile
@@ -0,0 +1,9 @@
+# This is a comment line
+CC=gcc
+
+correctness: correctness/CorrectnessTest.c
+	@echo "Testing correctness for $(lock):\n"
+	@${CC} correctness/CorrectnessTest.c -o build/test -fopenmp -include locks/$(lock).c
+	@./build/test $(thread_number) $(array_size)
+	@rm build/test
+
--- correctness/CorrectnessTest.c
+++ correctness/CorrectnessTest.c
@@ -1,15 +1,13 @@
-int main() {
+int main(int argc, char *argv[]) {
 
-    //omp_set_num_threads(8);
+    int thread_number = atoi(argv[1]);
+    omp_set_num_threads(thread_number);
+    int array_size = atoi(argv[2]);
 
     _Atomic int lock_calls = 0;
     int counter = 0;
 
-    int array_size = 10000;
     float array_sum_seqential = 0, array_sum_parallel = 0;
-
-    Lock LOCK;
-    init(&LOCK);
 
     // generate random array + sum it up sequentially
     float array[array_size];
@@ -17,6 +15,9 @@ int main() {
         array[i] = (float)rand() / (float)RAND_MAX;
         array_sum_seqential = array_sum_seqential + array[i];
     }
+
+    Lock LOCK;
+    init(&LOCK);
 
     printf("Hello from threads ...\n");
     
@@ -36,7 +37,6 @@ int main() {
 
             // Add elementwise
             array_sum_parallel += array[counter];
-            //printf("%d \n", omp_get_thread_num());
 
             // Thread is in critical section - write to counter 
             counter += 1; 
@@ -53,9 +53,9 @@ int main() {
     printf("Sequential sum:\t%.4f\nParallel sum:\t%.4f\n", array_sum_seqential, array_sum_parallel);
     printf("--------------------------------------------\n");
     if (array_sum_seqential != array_sum_parallel){
-        printf("ERROR: sums of arrays not equal -> correctness violated\n");
+        printf("ERROR: sums of arrays not equal - correctness violated!\n");
     } else {
-        printf("No violations in correctness found\n");
+        printf("No violations in correctness found!\n");
     }
 
     return 0;
